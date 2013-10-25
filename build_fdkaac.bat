efb5fd2f8874f29569afdb0fe06eea6a4a758a74
nu774 <honeycomb77@gmail.com>
2013-10-25 10:25:01 +0900

    automatically fetch AppMakefile and LibMakefile

--- build_fdkaac.bat
+++ build_fdkaac.bat
@@ -3,6 +3,7 @@ setlocal
 set PATH=%CD%\bin;%CD%\msys\1.0\bin
 
 if exist fdkaac-master.zip del fdkaac-master.zip
+wget --no-check-certificate https://raw.github.com/nu774/fdkaac_autobuild/master/files/AppMakefile -O files/AppMakefile
 wget --no-check-certificate https://github.com/nu774/fdkaac/archive/master.zip -O fdkaac-master.zip 
 if exist fdkaac-master rd /s /q fdkaac-master
 unzip fdkaac-master.zip
--- build_libfdk_aac.bat
+++ build_libfdk_aac.bat
@@ -3,6 +3,7 @@ setlocal
 set PATH=%CD%\bin;%CD%\msys\1.0\bin
 
 if exist fdk-aac-master.zip del fdk-aac-master.zip
+wget --no-check-certificate https://raw.github.com/nu774/fdkaac_autobuild/master/files/LibMakefile -O files/LibMakefile
 wget --no-check-certificate https://github.com/mstorsjo/fdk-aac/archive/master.zip -O fdk-aac-master.zip 
 if exist fdk-aac-master rd /s /q fdk-aac-master
 unzip fdk-aac-master.zip
