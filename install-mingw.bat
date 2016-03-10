f16c05b5ff33143b9cfc5fdd6ff0187e9ba3d410
nu774 <honeycomb77@gmail.com>
2016-03-10 19:16:07 +0900

    create hardlink of gcc.exe to cc.exe if cc.exe missing
    Seems like MinGW gcc-core-4.9.3-1 package doesn't include cc.exe

--- install-mingw.bat
+++ install-mingw.bat
@@ -1,2 +1,6 @@
 @echo off
 cscript //E:jscript install-mingw.js %*
+pushd bin
+if not exist cc.exe fsutil hardlink create cc.exe gcc.exe
+if not exist mingw32-cc.exe fsutil hardlink create mingw32-cc.exe mingw32-gcc.exe
+popd bin
